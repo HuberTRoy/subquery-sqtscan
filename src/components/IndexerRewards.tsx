7ec1368352ab42ded6c9aea81baab9347b7c2bce
PHANDUCHUY\duchu <duchuytb9x@gmail.com>
2024-10-02 22:40:27 +0700

    update indexerRewardsChart

--- src/components/IndexerRewards.tsx
+++ src/components/IndexerRewards.tsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { gql, useLazyQuery } from '@apollo/client';
 import { formatNumber, formatSQT, truncateAddress } from '@utils';
 import BigNumberJs from 'bignumber.js';
-import Highcharts from 'highcharts';
+import * as echarts from 'echarts';
 
 interface IndexerRewardsProps {
   era: number;
-  deploymentId: string;
+  indexerId: string;
 }
 
 interface ChartData {
@@ -21,179 +21,9 @@ interface ChartData {
   delegatorApy: number;
 }
 
-const darkTheme = {
-  chart: {
-    backgroundColor: '#66000000',
-    style: {
-      fontFamily: 'sans-serif',
-    },
-    plotBorderColor: '#606063',
-  },
-  title: {
-    style: {
-      color: '#E0E0E3',
-      textTransform: 'uppercase',
-      fontSize: '20px',
-    },
-  },
-  xAxis: {
-    gridLineColor: '#707073',
-    labels: {
-      style: {
-        color: '#E0E0E3',
-      },
-    },
-    lineColor: '#707073',
-    minorGridLineColor: '#505053',
-    tickColor: '#707073',
-    title: {
-      style: {
-        color: '#A0A0A3',
-      },
-    },
-  },
-  yAxis: {
-    gridLineColor: '#707073',
-    labels: {
-      style: {
-        color: '#E0E0E3',
-      },
-    },
-    lineColor: '#707073',
-    minorGridLineColor: '#505053',
-    tickColor: '#707073',
-    tickWidth: 1,
-    title: {
-      style: {
-        color: '#A0A0A3',
-      },
-    },
-  },
-  tooltip: {
-    backgroundColor: 'rgba(0, 0, 0, 0.85)',
-    style: {
-      color: '#F0F0F0',
-    },
-  },
-  plotOptions: {
-    series: {
-      dataLabels: {
-        color: '#B0B0B3',
-      },
-      marker: {
-        lineColor: '#333',
-      },
-    },
-    boxplot: {
-      fillColor: '#505053',
-    },
-    candlestick: {
-      lineColor: 'white',
-    },
-    errorbar: {
-      color: 'white',
-    },
-  },
-  legend: {
-    itemStyle: {
-      color: '#E0E0E3',
-    },
-    itemHoverStyle: {
-      color: '#FFF',
-    },
-    itemHiddenStyle: {
-      color: '#606063',
-    },
-  },
-  credits: {
-    style: {
-      color: '#666',
-    },
-  },
-  labels: {
-    style: {
-      color: '#707073',
-    },
-  },
-  drilldown: {
-    activeAxisLabelStyle: {
-      color: '#F0F0F3',
-    },
-    activeDataLabelStyle: {
-      color: '#F0F0F3',
-    },
-  },
-  navigation: {
-    buttonOptions: {
-      symbolStroke: '#DDDDDD',
-      theme: {
-        fill: '#505053',
-      },
-    },
-  },
-  rangeSelector: {
-    buttonTheme: {
-      fill: '#505053',
-      stroke: '#000000',
-      style: {
-        color: '#CCC',
-      },
-      states: {
-        hover: {
-          fill: '#707073',
-          stroke: '#000000',
-          style: {
-            color: 'white',
-          },
-        },
-        select: {
-          fill: '#000003',
-          stroke: '#000000',
-          style: {
-            color: 'white',
-          },
-        },
-      },
-    },
-    inputBoxBorderColor: '#505053',
-    inputStyle: {
-      backgroundColor: '#333',
-      color: 'silver',
-    },
-    labelStyle: {
-      color: 'silver',
-    },
-  },
-  navigator: {
-    handles: {
-      backgroundColor: '#666',
-      borderColor: '#AAA',
-    },
-    outlineColor: '#CCC',
-    maskFill: 'rgba(255,255,255,0.1)',
-    series: {
-      color: '#7798BF',
-      lineColor: '#A6C7ED',
-    },
-    xAxis: {
-      gridLineColor: '#505053',
-    },
-  },
-  scrollbar: {
-    barBackgroundColor: '#808083',
-    barBorderColor: '#808083',
-    buttonArrowColor: '#CCC',
-    buttonBackgroundColor: '#606063',
-    buttonBorderColor: '#606063',
-    rifleColor: '#FFF',
-    trackBackgroundColor: '#404043',
-    trackBorderColor: '#404043',
-  },
-};
-
 const FETCH_REWARDS_QUERY = gql`
-  query MyQuery($eraId: BigFloat!, $eraIdx: Int) {
-    indexerRewards(filter: { eraId: { equalTo: $eraId } }) {
+  query MyQuery($eraId: BigFloat!, $eraIdx: Int, $indexerId: String) {
+    indexerRewards(filter: { eraId: { equalTo: $eraId }, indexerId: { includes: $indexerId } }) {
       edges {
         node {
           amount
@@ -210,10 +40,9 @@ const FETCH_REWARDS_QUERY = gql`
                 delegatorStake
                 totalStake
               }
-              keys
             }
           }
-          indexerApySummaries(filter: { eraIdx: { equalTo: $eraIdx } }) {
+          indexerApySummaries(filter: { eraIdx: { equalTo: $eraIdx }, indexerId: { includes: $indexerId } }) {
             groupedAggregates(groupBy: INDEXER_ID) {
               sum {
                 delegatorApy
@@ -231,10 +60,9 @@ const FETCH_REWARDS_QUERY = gql`
   }
 `;
 
-const IndexerRewards: React.FC<IndexerRewardsProps> = ({ era, deploymentId }) => {
+const IndexerRewards: React.FC<IndexerRewardsProps> = ({ era, indexerId }) => {
   const [eraId, setEraId] = useState<number>(era);
-  const [indexerId, setIndexerId] = useState(deploymentId);
-  const [chartType, setChartType] = useState('column');
+  const [currentIndexerId, setIndexerId] = useState(indexerId);
   const [chartData, setChartData] = useState<ChartData[]>([]);
 
   interface IndexerRewardsData {
@@ -277,15 +105,14 @@ const IndexerRewards: React.FC<IndexerRewardsProps> = ({ era, deploymentId }) =>
 
   const [fetchRewards, { data: rewardsData }] = useLazyQuery<IndexerRewardsData>(FETCH_REWARDS_QUERY);
 
-  // Lắng nghe sự thay đổi của era và deploymentId
   useEffect(() => {
     setEraId(era);
-    setIndexerId(deploymentId);
-  }, [era, deploymentId]);
+    setIndexerId(indexerId);
+  }, [era, indexerId]);
 
   useEffect(() => {
-    fetchRewards({ variables: { eraId, eraIdx: era } });
-  }, [eraId, fetchRewards]);
+    fetchRewards({ variables: { eraId: eraId, eraIdx: era, indexerId: currentIndexerId } });
+  }, [eraId, currentIndexerId, fetchRewards]);
 
   useEffect(() => {
     if (rewardsData) {
@@ -302,7 +129,7 @@ const IndexerRewards: React.FC<IndexerRewardsProps> = ({ era, deploymentId }) =>
         indexerApy: parseFloat(data.indexerApy ?? '0') / Math.pow(10, 16),
         delegatorApy: parseFloat(data.delegatorApy ?? '0') / Math.pow(10, 16),
       }));
-      console.log(chartData);
+      console.log('chartData: ' + chartData);
       setChartData(chartData);
       renderChart(chartData);
     }
@@ -399,80 +226,136 @@ const IndexerRewards: React.FC<IndexerRewardsProps> = ({ era, deploymentId }) =>
   }
 
   const renderChart = (data: ChartData[]): void => {
-    Highcharts.setOptions(darkTheme);
-
-    Highcharts.chart('container', {
-      chart: {
-        type: chartType,
-        height: 600, // Đặt chiều cao của biểu đồ thành 600px
-      },
-      title: {
-        text: 'Node Operator and Delegator APY Era ' + eraId,
-      },
-      xAxis: {
-        categories: data.map((d) => d.name),
-      },
-      yAxis: [
-        {
-          title: {
-            text: 'SQT',
+    const chartDom = document.getElementById('container');
+    if (chartDom) {
+      const myChart = echarts.init(chartDom);
+      const option = {
+        title: {
+          text: '',
+          textStyle: {
+            color: '#E0E0E3',
           },
         },
-        {
-          title: {
-            text: 'APY',
+        tooltip: {
+          trigger: 'axis',
+          backgroundColor: 'rgba(0, 0, 0, 0.85)',
+          textStyle: {
+            color: '#F0F0F0',
           },
-          opposite: true,
+          axisPointer: {
+            type: 'shadow',
+          },
+          formatter: function (params: any[]) {
+            const indexerId = params[0].axisValue; // Lấy giá trị của trục X
+            let tooltipContent = `Indexer: ${indexerId}<br/>`; // Thêm dòng "Indexer" với giá trị indexerId trên đầu
+            params.forEach((param) => {
+              const value = param.seriesName.includes('APY') ? `${param.value}%` : param.value;
+              tooltipContent += `${param.marker} ${param.seriesName}: ${value}<br/>`;
+            });
+            return tooltipContent;
+          },
         },
-      ],
-      credits: {
-        enabled: false,
-      },
-      series: [
-        {
-          name: 'Total Stake',
-          data: data.map((d) => d.totalStake),
+        legend: {
+          textStyle: {
+            color: '#E0E0E3',
+          },
         },
-        {
-          name: 'Indexer Stake',
-          data: data.map((d) => d.indexerStake),
+        xAxis: {
+          type: 'category',
+          data: data.map((d) => d.name),
+          axisLine: {
+            lineStyle: {
+              color: '#707073',
+            },
+          },
+          axisLabel: {
+            color: '#E0E0E3',
+            rotate: 45, // Xoay nhãn trục X 45 độ
+          },
         },
-        {
-          name: 'Delegator Stake',
-          data: data.map((d) => d.delegatorStake),
-        },
-        {
-          name: 'Total Reward',
-          data: data.map((d) => d.totalReward),
-        },
-        {
-          name: 'Indexer Reward',
-          data: data.map((d) => d.indexerReward),
-        },
-        {
-          name: 'Delegator Reward',
-          data: data.map((d) => d.delegatorReward),
-        },
-        {
-          name: 'Indexer APY',
-          data: data.map((d) => d.indexerApy),
-          yAxis: 1,
-          type: 'line',
-        },
-        {
-          name: 'Delegator APY',
-          data: data.map((d) => d.delegatorApy),
-          yAxis: 1,
-          type: 'line',
-        },
-      ],
-    });
+        yAxis: [
+          {
+            type: 'value',
+            name: 'SQT',
+            axisLine: {
+              lineStyle: {
+                color: '#707073',
+              },
+            },
+            axisLabel: {
+              color: '#E0E0E3',
+            },
+          },
+          {
+            type: 'value',
+            name: 'APY',
+            axisLine: {
+              lineStyle: {
+                color: '#707073',
+              },
+            },
+            axisLabel: {
+              color: '#E0E0E3',
+            },
+            splitLine: {
+              show: false,
+            },
+          },
+        ],
+        series: [
+          {
+            name: 'Total Stake',
+            type: 'bar',
+            data: data.map((d) => d.totalStake),
+          },
+          {
+            name: 'Indexer Stake',
+            type: 'bar',
+            data: data.map((d) => d.indexerStake),
+          },
+          {
+            name: 'Delegator Stake',
+            type: 'bar',
+            data: data.map((d) => d.delegatorStake),
+          },
+          {
+            name: 'Total Reward',
+            type: 'bar',
+            data: data.map((d) => d.totalReward),
+          },
+          {
+            name: 'Indexer Reward',
+            type: 'bar',
+            data: data.map((d) => d.indexerReward),
+          },
+          {
+            name: 'Delegator Reward',
+            type: 'bar',
+            data: data.map((d) => d.delegatorReward),
+          },
+          {
+            name: 'Indexer APY',
+            type: 'line',
+            yAxisIndex: 1,
+            data: data.map((d) => d.indexerApy),
+          },
+          {
+            name: 'Delegator APY',
+            type: 'line',
+            yAxisIndex: 1,
+            data: data.map((d) => d.delegatorApy),
+          },
+        ],
+        backgroundColor: '#66000000',
+      };
+      myChart.setOption(option);
+    }
   };
 
   return (
     <div>
       <div id="main-content" className="row">
-        <div id="container"></div>
+        <div id="container" style={{ width: '100%', height: '600px' }}></div>
       </div>
     </div>
   );
